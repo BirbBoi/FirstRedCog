385df635fc1e7a8465f9044869dc02dbcd6a749d
Lauren <42990294+Kat-Tan@users.noreply.github.com>
2020-03-08 14:28:00 +0000

    Update testcog.py

--- katan/testcog.py
+++ katan/testcog.py
@@ -1,2 +1,10 @@
-import discord
+from redbot.core import commands
 
+class Mycog(commands.Cog):
+    """My custom cog"""
+
+    @commands.command()
+    async def mycom(self, ctx):
+        """This does stuff!"""
+        # Your code will go here
+        await ctx.send("I can do stuff!")
